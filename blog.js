80ad93796a27eae21d10541686c1c365082d2cc0
Jacob Porter Herold <dcejake@gmail.com>
2015-12-01 00:17:52 -0800

    fixed style in html and blog.js

--- blog.js
+++ blog.js
@@ -11,45 +11,37 @@ function printArticle(obj) {
 printArticle.prototype.toHtml = function () {
     var $newArticle = $('.articlePlaceholder').clone();
     $newArticle.removeClass('articlePlaceholder');
-    console.log("1");
     $newArticle.find('h1:first').html(this.title);
-    console.log("2");
     $newArticle.append('<hr>');
-    console.log("3");
     $newArticle.find('#authorSpot').html('<a href= '+this.authorUrl+'<p>Author: '+this.author + '</p></a>');
-    console.log("4");
     $newArticle.find('time').html('Published ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-    console.log("5");
     $newArticle.append('<hr>');
-    console.log("6");
     $newArticle.find('.articleContent').html(this.body);
-    console.log("7");
     $newArticle.append('<hr>');
-    console.log("8");
     return $newArticle;
 }
 
 
 $(function() {
 
-   function sortDate(A) {
-     A.sort(
+  function sortDate(A) {
+    A.sort(
       function(a, b) {
         if (a.publishedOn < b.publishedOn) { return 1; }
         if (a.publishedOn > b.publishedOn) { return -1; }
         return 0;
       }
     );
-   }
+  }
 
-   sortDate(blog.rawData);
+  sortDate(blog.rawData);
 
-   var dataArray = {};
-   for(ii=0; ii<blog.rawData.length; ii++){
-     dataArray = new printArticle(blog.rawData[ii]);
-     $('#articleWrapper').append(dataArray.toHtml());
-   }
- });
+  var dataArray = {};
+  for(ii=0; ii<blog.rawData.length; ii++){
+    dataArray = new printArticle(blog.rawData[ii]);
+    $('#articleWrapper').append(dataArray.toHtml());
+  }
+});
 
 
 // //sort rawdata array
