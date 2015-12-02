5e388f87325e8407a73c29a6c3d484b667702e88
Jacob Porter Herold <dcejake@gmail.com>
2015-12-01 16:33:45 -0800

    got all hide/show functionalaity

--- blog.js
+++ blog.js
@@ -32,12 +32,9 @@ $(function() {
       $newArticle.find('time').html('Published ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
       $newArticle.append('<hr>');
       $newArticle.find('.articleContent').html(this.body);
-      $newArticle.find('.articleContent').hide();
-      $newArticle.find('p:first').show();
-
-      // $newArticle.find('p:first').removeClass('minimize');
+      $newArticle.find('.articleContent').children().not('p:first').hide();
+      //$newArticle.find('p:first').show();
       $newArticle.find('.buttonAnchor').html('<br><button class="expandArticleText"> -> Expand Article </button><br>');
-      //console.log("button cretor firing");
       $newArticle.append('<hr><br>');
       $("#authorDropDownAnchor").append('<option value="'+this.author+'">'+this.author+'</option>');
       $("#categoryDropDownAnchor").append('<option value="'+this.category+'">'+this.category+'</option>');
@@ -54,61 +51,8 @@ $(function() {
 
   $(".expandArticleText").click(function(){
     console.log("Expand firing 1.0");
-    $(this).parent().prev().show();
-
+    $(this).parent().prev().children().show();
+    console.log("The JS interpret should've expanded now");
   });
 
 });
-
-// jQuery(function(){
-//
-//     var minimized_elements = $('.articleContent');
-//
-//     minimized_elements.each(function(){
-//         var t = $(this).text();
-//         if(t.length < 100) return;
-//
-//         $(this).html(
-//             t.slice(0,100)+'<span>... </span><a href="#" class="more">More</a>'+
-//             '<span style="display:none;">'+ t.slice(100,t.length)+' <a href="#" class="less">Less</a></span>'
-//         );
-//
-//     });
-//
-//     $('a.more', minimized_elements).click(function(event){
-//         event.preventDefault();
-//         $(this).hide().prev().hide();
-//         $(this).next().show();
-//     });
-//
-//     $('a.less', minimized_elements).click(function(event){
-//         event.preventDefault();
-//         $(this).parent().hide().prev().show().prev().show();
-//     });
-// });
-
-
-// //sort rawdata array
-// $(function() {
-//
-//    function sortArticles(A) {
-//      A.sort(
-//       function(a, b) {
-//         if (a.publishedOn.getTime() < b.publishedOn.getTime()) { return 1; }
-//         if (a.publishedOn.getTime() > b.publishedOn.getTime()) { return -1; }
-//         return 0;
-//       }
-//     );
-//    }
-//
-//    sortArticles(blog.rawData);
-//  });
-//
-//
-//
-// //print ALL THE THINGS!
-// $.each(blog.rawData, function(i) {
-//  $.each(blog.rawData[i], function(key,val) {
-//    $('#articleWrapper').append(val);
-//  });
-// });
