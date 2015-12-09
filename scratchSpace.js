47a0ba37430f089d178c23138b2548894875a607
Jacob Porter Herold <dcejake@gmail.com>
2015-12-09 09:44:35 -0800

    have word count and unique author array

---
+++ scratchSpace.js
@@ -0,0 +1,19 @@
+qq = [
+    {"author": "Tolkien", "body": "THE RING!", "date": "2015-12-07"},
+    {"author": "LeGuin", "body": "EARTHSEA!", "date": "2015-12-07" },
+    {"author": "Rowling",  "body": "EXPECTO PATRONUS", "date": "2015-12-07"},
+    {"author": "Rowling",  "body": "A CASUAL VACANCY", "date": "2015-12-07"},
+
+];
+
+// function findAllArticleWordLengths(arrayOfArticles){
+//
+// }
+
+
+
+var myCoolString = qq[0].body//(wordCount);
+arrayOfAwesomeWords = myCoolString.split(/\s+/);
+console.log(arrayOfAwesomeWords);
+
+console.log(arrayOfAwesomeWords.length);
--- stats.js
+++ stats.js
@@ -1,57 +1,182 @@
-// var localContent = localStorage.getItem('localArticleData');
-//
-// function getArrayOfBodies (element, index, array){
-//
-//   return $.map();
-// }
-//
-// bodies = localContent.forEach(getArrayOfBodies);
-
-qq = [
-    {"author": "Tolkien", "body": "THE RING!"},
-    {"author": "LeGuin", "body": "EARTHSEA!" },
-    {"author": "Rowling",  "body": "EXPECTO PATRONUS"},
-    {"author": "Rowling",  "body": "A CASUAL VACANCY"},
-
-];
-//MAKE FILTER
-//Goal: give author with articles associated. Articles have author, title, etc. Only
-function getPotter(A){
-  return A.author == "Rowling";  //Return info at index# only when true
-
-}
-// var searchResult = qq.filter(getPotter);
-// console.log(searchResult);
-
-
-function makeAuthorFilter(author){
-  var AuthorToSearchFor = author;
-  return function (A){
-    return A.author == AuthorToSearchFor;
-
-  }
-
-}
+ var localContent = $.getJSON('blogArticles.json', function(localContent)
+ {
+ console.log(localContent);
 
 
 
 
-//pass it ("author", "rowling")
+//pass it (makeSearchFilter("keytype", "value"));
+//example: var x = qq.filter(makeSearchFilter("author", "rowling"));
+//returns array that fits criteria
 function makeSearchFilter(keyType, value){
   var valueToSearchFor = value;
   return function (A){
     return A[keyType] == valueToSearchFor;
-
-
   }
 }
 
-// function wordCount(){
-//
+
+// var searchResult = localContent.filter(makeSearchFilter("category", "sports"));
+// console.log(searchResult);
+
+//getUnique takes an array arguement and parses out all redundant bits of the array, returning a clean//simple array back
+function getUnique (array) {
+  console.log("before =" + array);
+  var u = {},
+    a = [];
+  for (var i = 0, l = array.length; i < l; ++i) {
+    if (u.hasOwnProperty(array[i])) {
+      continue;
+    }
+    a.push(array[i]);
+    u[array[i]] = 1;
+  }
+  console.log("after =" + a);
+  return a;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function makeAuthorArray(nameOfAuthor){
+  var authArray = {};
+  authArray.author = nameOfAuthor.author;
+  return authArray;
+}
+function wordCount(rawArticles){
+  var countedWordsInArticle = $(rawArticles.body).text().split(' ').length;
+  return countedWordsInArticle
+}
+
+// function convertToMarkdown(a){
+//   if (a.markdown){
+//     a.body = marked(a.markdown);
+//   }
+//   return a;
 // }
-// returns true if harry potter article
-var searchResult = qq.filter(makeSearchFilter("body", "EARTHSEA!"));
 
-console.log(foundPotter);
 
-// var searchResuleWordCount = searchResult.map(wordCount);
+// var bodyValue            = $(localContent.body).text();
+// var characterCount = $(localContent.text).length;
+// console.log(characterCount);
+// var avgLettersPerWord = ((characterCount) /(bodyValue));
+// console.log(avgLettersPerWord);
+// var caniHazMarkDown = $.grep(localContent, function(my) {
+//    return my.markdown;
+//  });
+//
+//  $.each(caniHazMarkDown, function(key, value) {
+//    bodyValue += $(marked(value.markdown)).text();
+//  });
+//  var wordCount = bodyValue.split(/\s+/).length;
+// console.log(wordCount);
+
+//$wordCount.append(" " + wordCount + 'words written');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getStats (rawBrowserData){
+  var stats = stats || {};
+  console.log("JAKE! OVER HREE" + rawBrowserData);
+  var rawArticles = rawBrowserData;
+  console.log("These are the raw articles" + rawArticles);
+  var articlesContainingHtmlStill;
+  var articlesWordCount;
+  var wordLengthsTotal;
+  // var bodyValue            = $(blogData.body).text();
+
+  var arrayOfAuthors = rawArticles.map(makeAuthorArray)
+  //var uniqueAuthorNames = getUnique(arrayOfAuthors); //redundant
+
+  stats.numberArticles = rawArticles.length;
+  console.log("number of articles is: "+stats.numberArticles);
+
+  stats.numberAuthors = $.unique(arrayOfAuthors.map(function(A) { return A.author; })).length;
+  console.log("Number of unique authors is: "+stats.numberAuthors);
+
+  // articlesContainingHtmlStill = rawArticles.map(convertToMarkdown);
+
+  articlesWordCount = articlesContainingHtmlStill.map(articlesWordCount);
+  stats.wordLengthsTotal = articlesWordCount.reduce(sum);
+  wordLengthsTotal = articlesContainingHtmlStill.map(wordLengthsTotal);
+  stats.wordLengthAverage = Math.round(wordLengthsTotal.reduce(sum)/(wordLengthsTotal.length));
+  return stats;
+};
+
+
+//works in console: saving for postarity
+
+
+
+//var numberOfUniqueAuthors = $.unique(arrayOfAuthors.map(function(A) { return A.author; })).length
+
+
+
+
+
+
+// var arrayOfAuthors = localContent.map(makeAuthorArray);
+
+
+
+
+
+
+function totalLengthOfWordsInArticle (){
+
+}
+function averageWordLength(){
+
+}
+// var arrayWithWordCount = localContent.map(wordCount());
+// console.log(arrayWithWordCount);
+
+
+function sum (thingToBeCounted){
+  var total = 0;
+  forEach (thingToBeCounted, function (thingToBeCounted){
+    total += thingToBeCounted;
+  })
+  return total;
+}
+
+// var myCoolString = qq[0].body//(wordCount);
+// arrayOfAwesomeWords = myCoolString.split(/\s+/);
+// console.log(arrayOfAwesomeWords);
+//
+// console.log(arrayOfAwesomeWords.length);
+
+
+
+
+
+getStats(localContent);
+
+// var localContent = JSON.parse(localStorage.getItem('localArticleData'));
+// console.log(localContent);
+});//ends getJSON Callback
